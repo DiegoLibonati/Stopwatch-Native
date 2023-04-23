da21220869d86e1cc02bd7bebe9934d119993199
Die <dlibonati@leafnoise.io>
2023-04-23 10:37:46 -0300

    new skins

--- src/helpers/constants/data.ts
+++ src/helpers/constants/data.ts
@@ -11,4 +11,16 @@ export const skins = [
     static: require("../../../assets/classic_static.png"),
     dinamic: require("../../../assets/classic_gif.gif"),
   },
+  {
+    id: "clock3",
+    name: "Pixel Sand",
+    static: require("../../../assets/pixel-sand.png"),
+    dinamic: require("../../../assets/pixel-sand.gif"),
+  },
+  {
+    id: "clock4",
+    name: "Pikachu Run",
+    static: require("../../../assets/pikachu.png"),
+    dinamic: require("../../../assets/pikachu.gif"),
+  },
 ];
